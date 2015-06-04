4e3441c078840a43e446c2f0cba9f48505191a98
Debakel <git@mrtz.me>
2015-06-04 20:34:18 +0200

    Update Hochschulwahl 2015

--- lang/de_DE.php
+++ lang/de_DE.php
@@ -6,38 +6,41 @@ $lang['header'] = "Hochschulwahlen 2015/2016";
 
 $lang['js'] = '<strong>Ohne Java-Script geht hier leider nichts. Dafür berechnen wir auch alles nur auf deinem Gerät und senden deine Meinungen an keinen Server.<br />Deal?</strong>';
 
-$lang['einleitung'] = '
-	<p><strong>Deine Antworten werden nur auf deinem Gerät (Computer, Handy, etc.) ausgewertet und an keinen Server gesendet.</strong> Das heißt aber auch, dass deine Antworten verloren gehen, sobald du diese Seite neu lädst oder verlässt.</p>
-				
-<p>Bei den Hochschulwahlen 2015 werden nicht nur die Fachschaftsräte und der Fachbereichsrat, sondern auch die studentischen Vertreter*innen für das Studierendenparlament und die Universitätsversammlung gewählt.</p>
-				
-				<p><strong>15. Juni - 18. Juni 2015 (jeweils von 10:30-14:30 Uhr)</strong><br />
-				Mensa Stadtmitte oder Hörsaal-Medienzentrum Lichtwiese</p>
-	
-				<p>Dies ist keine Wahlempfehlung, sondern nur ein Informationsangebot über die aktuellen Positionen in der Hochschulpolitik an der TU Darmstadt.</p>
+$lang['einleitung'] = '<p>Jeden Sommer finden die Uniwahlen statt, bei der die Studierenden entscheidet, wer sie für das folgende Jahr vertreten soll. Hintergrundinformationen und Interviews mit allen antretenden Listen (quasi die "Parteien") findet ihr in der <a href="">Wahlausgabe des UNIVERSUM-Magazins</a>.</p>
+				<br>
+				<p align="center">
+				<strong>Die Wahl findet statt am<br>
+				9. und 10. Juni</strong><br />
+				in den <a href="wahllokale.html">Wahllokalen eurer Fakultät</a></p>
+				<br>
+<p>Dieses Jahr treten an: <a href="http://ghg-augsburg.de">GRÜNE Hochschulgruppe (CampusGrün)</a>, <a href="">JUSOS</a>, <a href="https://fb.me/DieListe.Augsburg">DIE LISTE</a>, <a href="">RCDS</a>, <a href="">Die Bewegung</a>, <a href="">AUStA</a>, <a href="">NoAUStA</a> und <a href="">LHG</a>, <a href="">NoAUStA</a> und <a href="">nah.gedacht</a>.</p>
+
+<p>Klickt euch jetzt durch den Wahl-O-Mat, um eure Meinung mit den Positionen der Listen zu vergleichen!</p>
+
+<p><strong>Hinweis: </strong>Wir speichern auf dieser Seite keine Daten. Deine Antworten werden nur auf deinem Gerät ausgewertet und an keinen Server gesendet. Das heißt aber auch, dass deine Antworten verloren gehen, sobald du diese Seite neu läds oder verlässt.</p>
 ';
 
 
-$lang['losgehts'] = "los geht's";
+$lang['losgehts'] = "los gehts";
 
 
 
 // second page
 $lang['gewichtung'] = 'Was ist dir besonders wichtig?';
 
-$lang['gtxt'] 			='<p>Hier kannst du beliebig viele Statements auswählen, die dir besonders wichtig sind. Sie werden dann doppelt gewichtet.</p>';
+$lang['gtxt'] 			='<p>Hier kannst du beliebig viele Statements auswählen, die dir besonders wichtig sind. Sie werden dann doppelt so viel berücksichtigt.</p>';
 
 
 $lang['next'] = 'Weiter zur Auswertung';
 
 
 // third page
-$lang['result'] = '<p>Der studi-mat ergab für dich folgendes Ergebnis. Unten findest du zu jeder Frage die Postionen mit Begründungen (klick auf die Frage) der einzelnen Listen. Dies ist keine Wahlempfehlung, sondern nur ein Informationsangebot über die aktuellen Positionen in der Hochschulpolitik an der TU Darmstadt.</p>';
+$lang['result'] = '<p>Der studi-mat ergab für dich folgendes Ergebnis. Unten findest du zu jeder Frage die Postionen der einzelnen Listen.</p>';
 
 
 
 $lang['furtherinfos'] = '<h3>Weitere Informationen zur Wahl</h3>
-				<p>Viele weitere Infos zur diesjährigen Wahl und zur Hochschulpolitik an deiner Uni findest du in der Wahlzeitung und auf der Webseite deines AStA. <a href="https://www.asta.tu-darmstadt.de/asta/de/themen/hochschulwahlen">www.asta.tu-darmstadt.de/asta/de/themen/hochschulwahlen</a></p>';
+				<p>Viele weitere Infos zur diesjährigen Wahl und zur Hochschulpolitik findest du in der Wahlausgabe des <a href="http://universum.pink">UNIVERSUM-Magazin</a> und auf der <a href="TODO">Webseite des AStA</a>.</p>';
 				
 				
 				
