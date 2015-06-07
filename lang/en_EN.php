44da48bc74ec8f4c0c43307c3977e8156f417f09
Debakel <git@mrtz.me>
2015-06-08 01:02:20 +0200

    Fix en_EN.php

--- lang/en_EN.php
+++ lang/en_EN.php
@@ -10,11 +10,11 @@ $lang['einleitung'] = '
 	<p><strong>We do not save any data on this site. All your answers stay on your device.</strong>
 	This does also mean, that you will loose all your results once you leave this page or reload it.</p>
 	
-	<p>With university elections 2015 you do not only have the chance to elect your "Fachschaftsräte" and  your representatives in the "Fachbereichsrat" but also your representatives in the "Studierendenparlament" and the "Universitätsversammlung"</p>
+	<p>With university elections 2015 you do not only have the chance to elect your "Fachschaftsräte" and  your representatives in the "Fakultätsrat" but also your representatives in the "Studentische Konvent" and the "Erweiterte Universitätsleitung"</p>
 				
 				
-				<p><strong>15th june - 18th june 2015 (10:30am - 14:30am)</strong><br />
-				cafeteria city center and  Hörsaal-Medienzentrum Lichtwiese</p>
+				<p><strong>9th june - 10th june 2015</strong><br />
+				</p>
 	
 				<p>This is no voting advice but only a offer to inform yourself about the current positions of all the participating groups.</p>
 ';
@@ -37,7 +37,7 @@ $lang['result'] = '<p>The studi-mat calculated the following result for you. Dow
 
 
 $lang['furtherinfos'] = '<h3>Further informations</h3>
-				<p>More informations about the elections you will find on the webpages of the groups (may not be in english) and the official website of your students council (AStA). <a href="https://www.asta.tu-darmstadt.de/asta/de/themen/hochschulwahlen">www.asta.tu-darmstadt.de/asta/de/themen/hochschulwahlen</a></p>';
+				<p>More informations about the elections you will find on the webpages of the groups (may not be in english) and the official website of your students council (AStA). <a href="https://www.asta.uni-augsburg.de">www.asta.uni-augsburg.de</a></p>';
 
 
 ?>
