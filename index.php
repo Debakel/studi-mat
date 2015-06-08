c2b19b14e0a5fd5a4f97511f9991f00e7ea74c76
Debakel <git@mrtz.me>
2015-06-08 15:37:14 +0200

    Update index.php Piwik Trackingcode

--- index.php
+++ index.php
@@ -229,6 +229,21 @@
 		<!--<script type="text/javascript" src="csv2json.php"></script>-->
 		<script type="text/javascript" src="js/meinungsvergleich.js"></script>
 		<script src="shariff/shariff.min.js"></script>
+<!-- Piwik -->
+<script type="text/javascript">
+  var _paq = _paq || [];
+  _paq.push(['trackPageView']);
+  _paq.push(['enableLinkTracking']);
+  (function() {
+    var u="//piwik.universum.pink/";
+    _paq.push(['setTrackerUrl', u+'piwik.php']);
+    _paq.push(['setSiteId', 2]);
+    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+    g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
+  })();
+</script>
+<noscript><p><img src="//piwik.universum.pink/piwik.php?idsite=2" style="border:0;" alt="" /></p></noscript>
+<!-- End Piwik Code -->
 
 	</body>
 </html>
